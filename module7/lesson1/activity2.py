15678816054ed7138b85a1c9884810aafa1d6d76
Aayan51 <aayanpolta@gmail.com>
2025-07-06 17:16:17 +0300

    submit

---
+++ module7/lesson1/activity2.py
@@ -0,0 +1,13 @@
+#Print a sentence
+print("Welcome to the world of programming")
+
+#Print a Number
+print(4)
+
+#Next Line after print
+print("Hello Python /n")
+
+#Print all the keyworfs present in Python
+print("Keywords in Python are.....")
+import keyword
+print(keyword.kwlist)
